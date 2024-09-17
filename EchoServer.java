fd061a8d054e802f882c40000809129e026bb018
Achraf Khribech <achraf.khribech@etu.univ-grenoble-alpes.fr>
2024-09-17 22:32:12 +0000

    adding EchoServer for test

---
+++ EchoServer.java
@@ -0,0 +1,25 @@
+public class EchoServer {
+    public static void main(String[] args) {
+        Broker broker = new Broker("EchoServer");
+
+        // Server accepts connections in a separate thread
+        new Thread(() -> {
+            while (true) {
+                Channel channel = broker.accept(12345);
+                if (channel != null) {
+                    new Task(broker, () -> {
+                        byte[] buffer = new byte[256];
+                        int bytesRead;
+                        try {
+                            while ((bytesRead = channel.read(buffer, 0, buffer.length)) != -1) {
+                                channel.write(buffer, 0, bytesRead);  // Echo back the data
+                            }
+                        } finally {
+                            channel.disconnect();
+                        }
+                    }).start();
+                }
+            }
+        }).start();
+    }
+}
