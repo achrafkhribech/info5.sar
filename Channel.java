7cd185af870b5cd722b5c79341187584824264c1
Achraf Khribech <achraf.khribech@etu.univ-grenoble-alpes.fr>
2024-09-30 19:16:48 +0000

    Add abstract class Channel.java

---
+++ Channel.java
@@ -0,0 +1,13 @@
+abstract class Channel {
+    // Method to read bytes from the channel
+    abstract int read(byte[] bytes, int offset, int length);
+
+    // Method to write bytes to the channel
+    abstract int write(byte[] bytes, int offset, int length);
+
+    // Method to disconnect the channel
+    abstract void disconnect();
+
+    // Method to check if the channel is disconnected
+    abstract boolean disconnected();
+}
