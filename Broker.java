66bc8f2a5497b5589fa39b59d57182e500c2afa5
Achraf Khribech <achraf.khribech@etu.univ-grenoble-alpes.fr>
2024-09-30 19:06:29 +0000

    add abstract class broker.java

---
+++ Broker.java
@@ -0,0 +1,17 @@
+abstract class Broker {
+    // Field to store the broker's name
+    protected String name;
+
+    // Constructor to initialize the broker's name
+    Broker(String name) {
+        this.name = name;
+    }
+
+    // Abstract method to accept a connection on a given port
+    abstract Channel accept(int port);
+
+    // Abstract method to connect to a given broker by name and port
+    abstract Channel connect(String name, int port);
+}
+
+   
