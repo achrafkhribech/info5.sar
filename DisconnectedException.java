85b1ebb97b9ba587de6b30c4430fa64d661b6ac8
Achraf Khribech <achraf.khribech@etu.univ-grenoble-alpes.fr>
2024-11-17 19:48:57 +0100

    Moved files to new directories

--- DisconnectedException.java
+++
@@ -1,6 +0,0 @@
-// Purpose: Exception thrown when the channel has been disconnected.
-class DisconnectedException extends RuntimeException {
-    public DisconnectedException() {
-        super(  "The channel has been disconnected");
-    }
-}
