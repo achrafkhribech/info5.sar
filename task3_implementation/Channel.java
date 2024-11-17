5dc48738e76f579e5256eea0a8001b09e5773fdd
achraf Khribech <106166500+achrafkhribech@users.noreply.github.com>
2024-11-17 19:59:41 +0100

    Create Channel.java

---
+++ task3_implementation/Channel.java
@@ -0,0 +1,72 @@
+public class Channel implements IChannel {
+
+	private CircularBuffer inputBuffer;
+	private CircularBuffer outputBuffer;
+	Channel remoteChannel;
+	ReadListener listener;
+	private boolean disconnected;
+
+	private Task writeTask;
+	private boolean dangling;
+
+	public Channel() {
+		inputBuffer = new CircularBuffer(64);
+		writeTask = new Task("Write Task");
+		disconnected = false;
+		dangling = false;
+	}
+
+	@Override
+	public boolean write(byte[] bytes, int offset, int length, WriteListener listener) {
+		// Event base
+		if (outputBuffer == null || disconnected || listener == null || dangling) {
+			return false;
+		}
+		writeTask.post(new WriteRunnable(bytes, offset, length, outputBuffer, this, listener));
+		return true;
+	}
+
+	@Override
+	public void disconnect(DisconnectListener listener) {
+		new Task("Disconnect Task").post(new Runnable() {
+			@Override
+			public void run() {
+				disconnected = true;
+				remoteChannel.dangling = true;
+
+				if (listener != null) {
+					listener.disconnected();
+				}
+			}
+		});
+	}
+
+	@Override
+	public boolean disconnected() {
+		return disconnected;
+	}
+
+	void connect(Channel connectChannel) {
+		this.remoteChannel = connectChannel;
+		connectChannel.remoteChannel = this;
+		this.outputBuffer = connectChannel.inputBuffer;
+		connectChannel.outputBuffer = this.inputBuffer;
+	}
+
+	@Override
+	public int read(byte[] bytes, int offset, int length) {
+		int bytesRead = 0;
+		while (bytesRead < length && !inputBuffer.empty()) {
+			byte value = inputBuffer.pull();
+			bytes[offset + bytesRead] = value;
+			bytesRead++;
+		}
+		return bytesRead;
+	}
+
+	@Override
+	public void setReadListener(ReadListener listener) {
+		this.listener = listener;
+	}
+
+}
